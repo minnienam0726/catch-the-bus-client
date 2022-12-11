d5bef5667bcf0e207ceba685cc04d27d4b7e0f19
Minji <minnienam0726@gmail.com>
2022-12-12 00:48:40 +0900

    refactor: 누락 추가

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text, TouchableOpacity, StyleSheet, Button } from "react-native";
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 import { requestLocationPermission } from "../utils/getPermission";
